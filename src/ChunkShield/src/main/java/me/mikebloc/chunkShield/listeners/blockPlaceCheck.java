1360f0a91c4638edc66d4f5721b3fe3ea58bfb9f
mikebloc <elv2049@gmail.com>
2026-09-16 22:57:06 -0500

    more height corrections

--- src/ChunkShield/src/main/java/me/mikebloc/chunkShield/listeners/blockPlaceCheck.java
+++ src/ChunkShield/src/main/java/me/mikebloc/chunkShield/listeners/blockPlaceCheck.java
@@ -237,7 +237,7 @@ public final class blockPlaceCheck implements Listener
 
                 if (environment == World.Environment.NORMAL)
                 {
-                    main.Global.minY = -59;
+                    main.Global.minY = -62;
                     main.Global.maxY = world.getMaxHeight();
                 }
                 else if (environment == World.Environment.NETHER)
