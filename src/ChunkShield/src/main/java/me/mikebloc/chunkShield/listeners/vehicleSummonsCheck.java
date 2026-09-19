6a0dc0953598a353f1ed1ae3dafa8533aa07feef
mikebloc <elv2049@gmail.com>
2026-09-19 15:12:54 -0500

    fixes #28. Proper removed vehicle loc
    The issue at hand is that the location was based on the vehicle in the Event that triggered a check. Not the one getting removed.

--- src/ChunkShield/src/main/java/me/mikebloc/chunkShield/listeners/vehicleSummonsCheck.java
+++ src/ChunkShield/src/main/java/me/mikebloc/chunkShield/listeners/vehicleSummonsCheck.java
@@ -27,10 +27,6 @@ public final class vehicleSummonsCheck implements Listener
     @EventHandler
     public void onVehicleCreate(VehicleCreateEvent e)
     {
-        int x = (int) e.getVehicle().getLocation().getX();
-        int y = (int) e.getVehicle().getLocation().getY();
-        int z = (int) e.getVehicle().getLocation().getZ();
-
         World world = e.getVehicle().getWorld();
 
         if (main.Global.configToggleVehicleRadiusCheck)
@@ -38,7 +34,7 @@ public final class vehicleSummonsCheck implements Listener
             if (main.Global.configCollectiveVehicletLimit == -1) return;
 
             vehicleGlobal.theVehicle = e.getVehicle();
-            vehicleRadiusScan(world, x, y, z);
+            vehicleRadiusScan(world);
         }
     }
 
@@ -46,10 +42,6 @@ public final class vehicleSummonsCheck implements Listener
     @EventHandler
     public void vehicleMoveCheck (VehicleMoveEvent e)
     {
-        int x = (int) e.getVehicle().getLocation().getX();
-        int y = (int) e.getVehicle().getLocation().getY();
-        int z = (int) e.getVehicle().getLocation().getZ();
-
         World world = e.getVehicle().getWorld();
 
         if (main.Global.configToggleVehicleRadiusCheck)
@@ -57,15 +49,17 @@ public final class vehicleSummonsCheck implements Listener
             if (main.Global.configCollectiveVehicletLimit == -1) return;
 
             vehicleGlobal.theVehicle = e.getVehicle();
-            vehicleRadiusScan(world, x, y, z);
+            vehicleRadiusScan(world);
         }
     }
 
+    public int x;
+    public int y;
+    public int z;
     /////////////////////////////////////////////////////////////////////////////
-    private void vehicleRadiusScan(World world, int x, int y, int z)
+    private void vehicleRadiusScan(World world)
     {
         main.Global.Entity_vehicleCount = 0;
-        // ---- Category totals: BOATS & MINE CARTS
         if (main.Global.configCollectiveVehicletLimit > 0)
         {
             int total = 0;
@@ -78,6 +72,9 @@ public final class vehicleSummonsCheck implements Listener
                     if (toRemove < 1)
                     {
                         entity.remove();
+                        x = (int) entity.getLocation().getX();
+                        y = (int) entity.getLocation().getY();
+                        z = (int) entity.getLocation().getZ();
                         main.Global.Entity_vehicleCount++;
                         entity.getLocation();
                         if (main.Global.Entity_vehicleCount < 10 && main.Global.configTogglePurgeEffect) world.spawnParticle(Particle.LAVA, entity.getLocation().toCenterLocation(), 4);
